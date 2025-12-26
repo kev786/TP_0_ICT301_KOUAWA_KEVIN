9b6a2f93cc7138d9eb46bc96d87b9bf2bcc6b420
kev786 <ryankevin756@gmail.com>
2025-12-26 19:40:41 +0100

    OCP : Implémentation après avec interface Shape pour l'extension

---
+++ OCP/apres_refactoring/AreaCalculator2.java
@@ -0,0 +1,7 @@
+package TP_0_ICT301_KOUAWA_KEVIN.OCP.apres_refactoring;
+
+public class AreaCalculator2 {
+	public double calculateArea(Shape shape) {
+		return shape.calculateArea();
+	}
+}
---
+++ OCP/apres_refactoring/Main.java
@@ -0,0 +1,8 @@
+package TP_0_ICT301_KOUAWA_KEVIN.OCP.apres_refactoring;
+
+public class Main {
+	public static void main(String[] args) {
+		Shape shape = new Rectangle(4, 3);
+		System.out.println("Area =  '" + shape.calculateArea() + "'");
+	}
+}
